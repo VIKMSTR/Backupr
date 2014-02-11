66745285f6adf2e5c5bacff5626d0aa2920af5c9
Viktor Krejčíř <krejcirviktor@gmail.com>
2014-02-11 23:32:01 +0100

    Updated with files

---
+++ Backupr/Analyze/Analyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backupr.Analyze
+{
+    class Analyzer
+    {
+        private static Analyzer _instance;
+
+        private Analyzer()
+        {
+            LoadDirectories();
+            AnalyzeChanges();
+        }
+
+        public void AnalyzeChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Analyzer GetAnalyzer()
+        {
+            return _instance ?? (_instance = new Analyzer());
+        }
+    }
+}
---
+++ Backupr/Core.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Timers;
+using Backupr.Analyze;
+
+namespace Backupr
+{
+    
+    class Core
+    {
+        private static Core _instance;
+        private Timer _t;
+        private const int Interval = 5000;
+        private readonly Analyzer _analyzer;
+
+        private Core()
+        {
+            _t = new Timer();
+            _t.Elapsed += Check;
+            _t.Interval = Interval;
+            _analyzer = Analyzer.GetAnalyzer();
+        }
+
+        private void Check(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            //analyzes the changes in directories
+            _analyzer.AnalyzeChanges();
+
+        }
+
+        public static Core AccessCore()
+        {
+            if (_instance == null)
+            {
+                _instance = new Core();
+            }
+            return _instance;
+        }
+
+    
+    }
+}
