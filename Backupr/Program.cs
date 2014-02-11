f53c704211c2f5e235e4f6878b2eb2957e5f6f81
Viktor Krejčíř <krejcirviktor@gmail.com>
2014-02-11 23:11:39 +0100

    Created first classes

---
+++ Backupr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backupr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0].Equals("-g"))
+                {
+                    //launch gui
+                    Console.WriteLine("launching gui");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No arguments specified, launched with latest settings in console mode");
+            }
+        }
+    }
+}
